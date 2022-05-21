8df6ef5b7f4caec16ca13b90a6c689e148d5c57b
Ananay <ananay98@gmail.com>
2022-05-21 12:49:57 +0530

    BUGFIX: TILED_Repeated working without gap

--- display_image.py
+++ display_image.py
@@ -17,14 +17,14 @@ def image_tiled(image, mode='-TILED_Repeated-', row_repeat=2,  col_repeat=2):
     i_width  = width-1
     i_height = height-1
 
+    big_image = np.zeros(((row_repeat*i_width)+1, (col_repeat*i_height)+1, 3), np.uint8)
     if mode=='-TILED_Repeated-':
-        big_image = np.zeros(((row_repeat*width), (col_repeat*height), 3), np.uint8)
-        for i in range(row_repeat):
-            for j in range(col_repeat):
-                big_image[i*width:(i+1)*width, j*height:(j+1)*height][:] = image
+        big_image[0*i_width:(1)*i_width  , 0*i_height:(1)*i_height  ][:] = crop_image(image, width=i_width, height=i_height)    #LU
+        big_image[1*i_width:(2)*i_width+1, 0*i_height:(1)*i_height  ][:] = crop_image(image, height=i_height)                   #RU
+        big_image[0*i_width:(1)*i_width  , 1*i_height:(2)*i_height+1][:] = crop_image(image, width=i_width)                     #LB
+        big_image[1*i_width:(2)*i_width+1, 1*i_height:(2)*i_height+1][:] = image                                                #RB
 
     elif mode=='-TILED_Rotated-':
-        big_image = np.zeros(((row_repeat*i_width)+1, (col_repeat*i_height)+1, 3), np.uint8)
         # doesn't factor row_repeat and col_repeat into account, right now - TODO
         horizontally_flipped_image = np.flip(image, 1)
         veritcally_flipped_image   = np.flip(image, 0)
